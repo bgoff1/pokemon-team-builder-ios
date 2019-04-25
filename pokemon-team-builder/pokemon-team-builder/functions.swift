91f472f9547bdcbaaf91e37732f1fcbe99d9b6a8
Sam Dillin <Sam@LAPINTOSH.local>
2019-04-25 14:24:02 -0500

    added function file getImageFromString moved into

---
+++ pokemon-team-builder/pokemon-team-builder/functions.swift
@@ -0,0 +1,17 @@
+//
+//  functions.swift
+//  pokemon-team-builder
+//
+//  Created by Sam Dillin on 4/25/19.
+//  Copyright © 2019 Judah Goff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+func getImageFromString(_ text:String) -> UIImage{
+    
+    let dataDecoded = Data(base64Encoded: text, options: .ignoreUnknownCharacters)!
+    let decodedImage = UIImage(data: dataDecoded)!
+    
+    return decodedImage
+}
